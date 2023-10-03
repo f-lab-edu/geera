0fa0e14592d3f7a03a1c689c68e0038fdda2f099
이승민 <iseungmin@iseungmin-ui-MacBookAir.local>
2023-10-03 21:48:45 +0900

    feat:프로젝트 생성

---
+++ src/main/java/com/seungminyi/geera/GeeraApplication.java
@@ -0,0 +1,13 @@
+package com.seungminyi.geera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeraApplication.class, args);
+	}
+
+}
