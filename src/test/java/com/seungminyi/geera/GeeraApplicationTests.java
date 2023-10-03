b2fada0c51754e753bd870c071e6deb624ca18d8
이승민 <iseungmin@iseungmin-ui-MacBookAir.local>
2023-10-03 21:48:45 +0900

    feat:프로젝트 생성

---
+++ src/test/java/com/seungminyi/geera/GeeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungminyi.geera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
