0e204ce28a7f99b9589cd55af3831a50397fcd2f
seungminyi <seungminyi95@gmail.com>
2023-10-20 16:23:27 +0900

    fix:스키마 sql 삭제

--- src/test/resources/schema/schema.sql
+++
@@ -1,9 +0,0 @@
--- MEMBER Table Create SQL
--- 테이블 생성 SQL - MEMBER
-CREATE TABLE MEMBER
-(
-    `id`        VARCHAR(255)    NULL        COMMENT '아이디',
-    `password`  VARCHAR(255)    NULL        COMMENT '패스워드',
-    `name`      VARCHAR(50)     NULL        COMMENT '이름',
-    PRIMARY KEY (id)
-);
