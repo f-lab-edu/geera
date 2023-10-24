40067276d4a6d777ec4381c8061439d57e24fcfb
seungminyi <seungminyi95@gmail.com>
2023-10-24 14:24:19 +0900

    fix:불필요한 코드 삭제 쿼리 가독성 개선 등 리뷰반영

--- src/test/java/com/seungminyi/geera/member/MemberServiceTest.java
+++ src/test/java/com/seungminyi/geera/member/MemberServiceTest.java
@@ -3,33 +3,35 @@ package com.seungminyi.geera.member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class MemberServiceTest {
 
-    private MemberService memberService;
+    @Mock
     private MemberRepository memberRepository;
+    @Mock
     private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    public void setUp() {
-        memberRepository = mock(MemberRepository.class);
-        passwordEncoder = mock(PasswordEncoder.class);
-        memberService = new MemberService(memberRepository, passwordEncoder);
-    }
+    @InjectMocks
+    private MemberService memberService;
 
     @Test
     public void registerMemberTest() {
+        String testEmail = "test@example.com";
+        String testPassword = "password1!";
+        String testName = "Test User";
         Member member = new Member();
-        member.setEmail("test@example.com");
-        member.setPassword("password1!");
-        member.setName("Test User");
-        when(passwordEncoder.encode(member.getPassword())).thenReturn("encodedPassword");
+        member.setEmail(testEmail);
+        member.setPassword(testPassword);
+        member.setName(testName);
 
+        when(passwordEncoder.encode(member.getPassword())).thenReturn("encodedPassword");
         memberService.registerMember(member);
 
         verify(passwordEncoder).encode("password1!");
