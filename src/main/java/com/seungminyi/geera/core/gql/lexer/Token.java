9f3c7e4459b26eddd02f123f309b1ea46d7cf008
seungminyi <seungminyi95@gmail.com>
2023-11-22 11:14:30 +0900

    fix:code style 들여쓰기 탭 -> 스페이스

--- src/main/java/com/seungminyi/geera/core/gql/lexer/Token.java
+++ src/main/java/com/seungminyi/geera/core/gql/lexer/Token.java
@@ -7,70 +7,70 @@ import lombok.ToString;
 @ToString
 public class Token {
 
-	public final TokenClass tokenClass;
-	public final String data;
-	public final Position position;
+    public final TokenClass tokenClass;
+    public final String data;
+    public final Position position;
 
-	public Token(TokenClass tokenClass, String data, int lineNum, int colNum) {
-		this.tokenClass = tokenClass;
-		switch (tokenClass) {
-			case STRING_LITERAL:
-				this.data = data.substring(1, data.length() - 1);
-				break;
-			case INT_LITERAL:
-				this.data = data;
-				break;
-			default:
-				this.data = "";
-		}
-		this.position = new Position(lineNum, colNum);
-	}
+    public Token(TokenClass tokenClass, String data, int lineNum, int colNum) {
+        this.tokenClass = tokenClass;
+        switch (tokenClass) {
+            case STRING_LITERAL:
+                this.data = data.substring(1, data.length() - 1);
+                break;
+            case INT_LITERAL:
+                this.data = data;
+                break;
+            default:
+                this.data = "";
+        }
+        this.position = new Position(lineNum, colNum);
+    }
 
-	public Token(TokenClass tokenClass, int lineNum, int colNum) {
-		this(tokenClass, "", lineNum, colNum);
-	}
+    public Token(TokenClass tokenClass, int lineNum, int colNum) {
+        this(tokenClass, "", lineNum, colNum);
+    }
 
-	public enum TokenClass {
+    public enum TokenClass {
 
-		// delimiters
-		LPAR,  // '('
-		RPAR,  // ')'
-		COMMA, // ','
+        // delimiters
+        LPAR,  // '('
+        RPAR,  // ')'
+        COMMA, // ','
 
-		// op
-		LT, // '<'
-		GT, // '>'
-		ASSIGN, // '='
-		LE, // "<="
-		GE, // ">="
-		IS, // "IS"
-		NOT, // "NOT"
-		IN, // "IN"
-		NOT_IN, // "NOT IN"
+        // op
+        LT, // '<'
+        GT, // '>'
+        ASSIGN, // '='
+        LE, // "<="
+        GE, // ">="
+        IS, // "IS"
+        NOT, // "NOT"
+        IN, // "IN"
+        NOT_IN, // "NOT IN"
 
-		// value
-		STRING_LITERAL, // \".*\"
-		INT_LITERAL,    // ('0'|...|'9')+
+        // value
+        STRING_LITERAL, // \".*\"
+        INT_LITERAL,    // ('0'|...|'9')+
 
-		// keyword
-		AND, // "AND"
-		OR, // "OR"
+        // keyword
+        AND, // "AND"
+        OR, // "OR"
 
-		// field
-		ISSUE_ID, // "ISSUE_ID"
-		PROJECT_ID, // "PROJECT_ID"
-		ISSUE_TYPE, // "ISSUE_TYPE"
-		ISSUE_STATUS, // "ISSUE_STATUS"
-		ISSUE_DESCRIPTION, // "ISSUE_DESCRIPTION"
-		ISSUE_DETAIL, // "ISSUE_DETAIL"
-		ISSUE_CONTRACT_ID, // "ISSUE_CONTRACT_ID"
-		ISSUE_REPORTER_ID, // "ISSUE_REPORTER_ID"
-		ISSUE_PRIORITY, // "ISSUE_PRIORITY"
-		SPRINT_ID, // "SPRINT_ID"
-		CREATE_AT, // "CREATE_AT"
-		TOP_ISSUE, // "TOP_ISSUE"
+        // field
+        ISSUE_ID, // "ISSUE_ID"
+        PROJECT_ID, // "PROJECT_ID"
+        ISSUE_TYPE, // "ISSUE_TYPE"
+        ISSUE_STATUS, // "ISSUE_STATUS"
+        ISSUE_DESCRIPTION, // "ISSUE_DESCRIPTION"
+        ISSUE_DETAIL, // "ISSUE_DETAIL"
+        ISSUE_CONTRACT_ID, // "ISSUE_CONTRACT_ID"
+        ISSUE_REPORTER_ID, // "ISSUE_REPORTER_ID"
+        ISSUE_PRIORITY, // "ISSUE_PRIORITY"
+        SPRINT_ID, // "SPRINT_ID"
+        CREATE_AT, // "CREATE_AT"
+        TOP_ISSUE, // "TOP_ISSUE"
 
-		EOF
-	}
+        EOF
+    }
 
 }
